c384a0006953a8a31313b63060c7d0bf0d36c5e6
lorenacimpean <lorenacimpean@gmail.com>
2022-06-09 22:47:42 +0300

    duamnele ajuta

--- garden_concert.ino
+++ garden_concert.ino
@@ -1,31 +1,46 @@
 
 #include <MIDI.h>
+#include <AsyncTimer.h>
 
-// analog pin 0
-const byte outputPin = A0;
-const int readDelay = 200;
+#define outputPin A0
+#define readDelay 200
+// http://www.phys.unsw.edu.au/jw/notes.html
+// midi notes from 21 - 108 - adjust later
+// min 12, max 87 - adjust later
+#define midiMinNote 21
+#define midiNotes 87
+
 MIDI_CREATE_DEFAULT_INSTANCE();
+// default minimum sensor value
+int sensorMin = 0;
+// default maximum sensor value
+int sensorMax = 1023;
 // value from reading
 int sensorValue;
-// maximum sensor value
-int sensorMax = 1023;
-// minimum sensor value
-int sensorMin = 0;
-
-void setup() {
-  // initialize serial communication at 9600 bits per second (debug):
-  Serial.begin(9600);
-  // Set MIDI baud rate:
-  // Serial.begin(31250);
-  // check if async calibrate with interval runs forever
-}
-
-void loop() {
-  readSensor();
-
-}
+AsyncTimer timer;
 
 // WIP
+void calibrate() {
+  int instantRead = analogRead(outputPin);
+  // record the maximum sensor value
+  if (instantRead > sensorMax) {
+    sensorMax = instantRead;
+  }
+  // record the minimum sensor value
+  if (instantRead < sensorMin) {
+    sensorMin = instantRead;
+  }
+  delay(100);
+}
+
+int getPitchFromReading() {
+  int diff = sensorValue - sensorMin;
+  if (diff < 0) {
+    diff = - diff;
+  }
+  int note = diff % midiNotes + midiMinNote;
+  return note;
+}
 
 void readSensor() {
   // read output pin
@@ -33,47 +48,40 @@ void readSensor() {
   // in case the sensor value is outside the range seen during calibration
   sensorValue = constrain(sensorValue, sensorMin, sensorMax);
   // apply the calibration to the sensor reading
-  sensorValue = map(sensorValue, sensorMin, sensorMax, 0, 255);
+  sensorValue = map(sensorValue, sensorMin, sensorMax, 0, 1023);
   // print output
   Serial.println(sensorValue);
   // delay in between reads for stability
-  delay(readDelay);
 }
 
 void sendNote() {
   // Send a Note (pitch 42, velo 127 on channel 1)
-  MIDI.sendNoteOn(42, 127, 1);
-  // Wait for a second -> change interval later
-  delay(1000);
+  int pitch = getPitchFromReading();
+  int velocity = (int) random(0, 128);
+  int channel = 1;
+  MIDI.sendNoteOn(pitch, velocity, channel);
+  //we can make this random
+  delay(100);
   // Stop the note
   MIDI.sendNoteOff(42, 0, 1);
 }
 
-void calibrate() {
-  // change to async and call at 5s(?) intervals
-  while (millis() < 5000) {
-    sensorValue = analogRead(sensorPin);
-
-    // record the maximum sensor value
-    if (sensorValue > sensorMax) {
-      sensorMax = sensorValue;
-    }
-
-    // record the minimum sensor value
-    if (sensorValue < sensorMin) {
-      sensorMin = sensorValue;
-    }
-  }
-  // set notes based on min max
+void setup() {
+  // initialize serial communication at 9600 bits per second (debug):
+  Serial.begin(9600);
+  // Set MIDI baud rate:
+  // Serial.begin(31250);
+  // sets a timer and calls calibrate() at 5s intervals hopefully forever
+  timer.setInterval([]() {
+    calibrate();
+  }, 5000);
 }
 
-void computeVelocity() {
-  // async reading in between readings - 15? 20? millis
-  // sort values
-  // count duplicates
-  // set velocity based on duplicate count
-  // max velocity = 127; min ?? 0;
-  // step = 127 / 15 (this read interval + maybe add some random to spice things up);
-  // velocity = step * duplicates count;
-  // add values to map: {note, velocity}
+void loop() {
+  timer.handle();
+  // sets a timer and calls calibrate() with 200milli timeout
+  timer.setTimeout([]() {
+    readSensor();
+    sendNote();
+  }, 200);
 }
